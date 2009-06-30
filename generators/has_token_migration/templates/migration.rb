f357f95775707a449689889376f3998312af40bb
laserlemon <steve@laserlemon.com>
2009-06-30 09:04:38 -0400

    Built a migration generator for application-wide unique tokens.

---
+++ generators/has_token_migration/templates/migration.rb
@@ -0,0 +1,17 @@
+class CreateTokens < ActiveRecord::Migration
+  def self.up
+    create_table :tokens do |t|
+      t.belongs_to :parent, :polymorphic => true
+      t.string :value
+    end
+    
+    change_table :tokens do |t|
+      t.index [:parent_id, :parent_type], :unique => true
+      t.index :value, :unique => true
+    end
+  end
+  
+  def self.down
+    drop_table :tokens
+  end
+end
