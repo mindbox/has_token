c756e0691d2df1f556a6dff88c06c83b13a5d6a4
laserlemon <steve.richert@gmail.com>
2009-10-22 09:46:36 -0400

    Removed the "for" named_scope as it wasn't in use (and really not all that handy).

--- lib/token.rb
+++ lib/token.rb
@@ -1,8 +1,6 @@
 class Token < ActiveRecord::Base
   belongs_to :parent, :polymorphic => true
 
-  named_scope :for, lambda{|k| {:conditions => {:parent_type => k.to_s.classify}} }
-
   def self.get(value)
     find_by_value(value).try(:parent)
   end
